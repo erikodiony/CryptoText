a6fad01ae3a840be0a27d8c92d8d863834ee8c0b
Erikodiony@live.com <eriko1994>
2018-06-25 20:44:14 +0700

    Adding Owner Build

--- app/src/main/java/tk/ngezz/cryptotext/FragmentAbout.java
+++ app/src/main/java/tk/ngezz/cryptotext/FragmentAbout.java
@@ -3,9 +3,12 @@ package tk.ngezz.cryptotext;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Erikodiony on 3/1/2018.
@@ -21,6 +24,11 @@ public class FragmentAbout extends Fragment {
         /* Define Your Functionality Here
            Find any view  => v.findViewById()
           Specifying Application Context in Fragment => getActivity() */
+
+        TextView tLink1 = (TextView) v.findViewById(R.id.PI_Title1_Body3);
+        tLink1.setText(Html.fromHtml("<a href=http://erikodiony.github.io> ERIKODIONY ARIESSA WAHYUDI"));
+        tLink1.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         return v;
     }
